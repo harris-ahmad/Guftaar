ad6a297489f01e5933f41a035fd1b8fdc526db4c
Emaan Atique <emaanatique812@gmail.com>
2023-04-08 21:49:52 +0500

    strength statements fixed

--- frontend/src/components/quick_practice.js
+++ frontend/src/components/quick_practice.js
@@ -3,13 +3,15 @@ import NavbarClient from './client_navbar';
 import cross from "../images/cross.svg";
 import mic from "../images/microphone.svg";
 import { useNavigate } from 'react-router-dom';
+import { ReactMicRecord } from 'react-mic-record';
+import { useState, useRef } from "react";
 
 function Quick_Practice(){
     const navigate = useNavigate();
+   
     function GoBack(){
         navigate("/client/dashboard")
     }
-
     return(
       <div className='quickBack'>
        <NavbarClient />
@@ -20,10 +22,8 @@ function Quick_Practice(){
             <h3 className='paragraphtext'>hello hey hi this is some random text that you can try and say goodluck i am just trying to make it longer to test if it gets kharab or not i am failing everything lol issok dr zafar hates me</h3>
         </div>
         <div className='iconback'>
-        <img src={mic} className='recordingmic'></img>
-        </div>
-       
-      </div>
+          <img src={mic} className='recordingmic'></img></div>
+          </div>
     );
 }
 

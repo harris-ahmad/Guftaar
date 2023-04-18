ac0ac034f3d918e41242f4de86a0f129cbeec453
harris-ahmad <24100315@lums.edu.pk>
2023-04-19 01:24:24 +0500

    formatting + unecessary imports removed

--- frontend/src/components/client_dashboard.js
+++ frontend/src/components/client_dashboard.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './client_dashboard.css'
-import qu from "../images/quote.svg"
-import phone from "../images/phone.svg"
-import course from "../images/course.svg"
-import arrow from "../images/arrow.svg"
-import happy from "../images/happy.svg"
-import moderate from "../images/moderate.svg"
-import extreme from "../images/extreme.svg"
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./client_dashboard.css";
+import qu from "../images/quote.svg";
+import phone from "../images/phone.svg";
+import course from "../images/course.svg";
+import arrow from "../images/arrow.svg";
+import happy from "../images/happy.svg";
+import moderate from "../images/moderate.svg";
+import extreme from "../images/extreme.svg";
+import { useNavigate } from "react-router-dom";
 import fire from "../images/fire.svg";
 import volume from "../images/volume.svg";
 import mic from "../images/mic.svg";
-import dailyActivities from './daily_activities';
-import NavbarClient from './client_navbar';
+import dailyActivities from "./daily_activities";
+import NavbarClient from "./client_navbar";
 import axios from "axios";
 
 function Quote() {
   const navigate = useNavigate();
-  const [quote, setQuote] = useState('');
+  const [quote, setQuote] = useState("");
   const [firstName, setFirstName] = useState("");
-  const [joiningDate, setJoiningDate] = useState("")
-  const [logRecord, setRecord] = useState ()
-  const [activeCourse, setCourse] = useState("")
-  const [hasCourse, setHasCourse] = useState(true); 
+  const [joiningDate, setJoiningDate] = useState("");
+  const [logRecord, setRecord] = useState();
+  const [activeCourse, setCourse] = useState("");
+  const [hasCourse, setHasCourse] = useState(true);
 
-  const email = localStorage.getItem("email")
+  const email = localStorage.getItem("email");
 
   useEffect(() => {
     const fetchData = async function () {
@@ -41,91 +41,106 @@ function Quote() {
         "http://localhost:4000/client/getClientDashboardDetails",
         toSend
       );
-      console.log(result2.data)
+      console.log(result2.data);
       setFirstName(result2.data.firstName);
-      setCourse(result2.data.currentActiveCourse); 
-      if(course.length > 0){
-        setHasCourse(false); 
+      setCourse(result2.data.currentActiveCourse);
+      if (course.length > 0) {
+        setHasCourse(false);
       }
-      // alert(hasCourse); 
+      // alert(hasCourse);
     };
     fetchData();
   }, []);
 
-  
+  function NoStuttering() {
+    axios.post("http://localhost:4000/client/moodlog", {
+      email: email,
+      mood: "NoStuttering",
+    });
+  }
+  function Moderate() {
+    axios.post("http://localhost:4000/client/moodlog", {
+      email: email,
+      mood: "Moderate",
+    });
+  }
+  function Extreme() {
+    axios.post("http://localhost:4000/client/moodlog", {
+      email: email,
+      mood: "Extreme",
+    });
+  }
 
-  function NoStuttering(){
-    axios.post('http://localhost:3000/client/moodlog', {email:email, mood: "NoStuttering"})
-}
-  function Moderate(){
-    axios.post('http://localhost:3000/client/moodlog', {email:email, mood: "Moderate"})
-}
-  function Extreme(){
-    axios.post('http://localhost:3000/client/moodlog', {email:email, mood: "Extreme"})
-}
- 
   return (
     <div className="client-bg">
-      <NavbarClient/>
-      <div className='mainwelcome'>
-        <h6 className='welcome'>{firstName}'s Dashboard</h6>
+      <NavbarClient />
+      <div className="mainwelcome">
+        <h6 className="welcome">{firstName}'s Dashboard</h6>
       </div>
-        <div className="quoteback">
-        <img className="qu"src={qu}/>
-        <h6 className='top1'>Strength Statements</h6>
-        <p className='text1'>{quote}</p>  
-        </div>
-       
-        <div className='log'>
-        <p className='logtext1'> How was your stuttering today?</p>
-          <div className='circleBack'></div>
-          <img className='happy'src={happy} onClick={NoStuttering}/> 
-       
-        <div className='circleBack2'></div>
-        <img className='meh'src={moderate} onClick={Moderate}/>
+      <div className="quoteback">
+        <img className="qu" src={qu} />
+        <h6 className="top1">Strength Statements</h6>
+        <p className="text1">{quote}</p>
+      </div>
 
-        <div className='circleBack3'></div>
-        <img className='sad'src={extreme} onClick={Extreme}/>
+      <div className="log">
+        <p className="logtext1"> How was your stuttering today?</p>
+        <div className="circleBack"></div>
+        <img className="happy" src={happy} onClick={NoStuttering} />
 
-        </div>
-        <p className='support1'>Guided Speech Support</p>
-        <div className='gbox1'>  
-        <img className='phone'src={phone}/>
-        <h6 className='subtext'>Coaching With Harris</h6>
-        <h6 className='detail'>Meetings</h6>
-        </div>
+        <div className="circleBack2"></div>
+        <img className="meh" src={moderate} onClick={Moderate} />
 
-        <div className='gbox2'>  
-        <img className='course'src={course}/>
-        {hasCourse ? <h6 className='subtext'>{course}</h6> : <h6 className='subtext'>Buy Courses</h6>}
-        <h6 className='detail'>Courses</h6>
-        <img className='arrow'src={arrow}/>
-        </div>
+        <div className="circleBack3"></div>
+        <img className="sad" src={extreme} onClick={Extreme} />
+      </div>
+      <p className="support1">Guided Speech Support</p>
+      <div className="gbox1">
+        <img className="phone" src={phone} />
+        <h6 className="subtext">Coaching With Harris</h6>
+        <h6 className="detail">Meetings</h6>
+      </div>
 
-        <div>
-          <div className='dailyTasks'>
-            <div className='rectangle'></div>
-            <h6 className='activitytext'>Daily Activities</h6>
-            <img className='icon'src={fire}></img>
-            <a href='dailyActivities'><img className='arrow2'src={arrow}/></a>
-          </div>
+      <div className="gbox2">
+        <img className="course" src={course} />
+        {hasCourse ? (
+          <h6 className="subtext">{course}</h6>
+        ) : (
+          <h6 className="subtext">Buy Courses</h6>
+        )}
+        <h6 className="detail">Courses</h6>
+        <img className="arrow" src={arrow} />
+      </div>
 
-          <div className='speechTechniques'>
-          <div className='rectangle2'></div>
-          <h6 className='activitytext'>Speech Techniques</h6>
-          <img className='icon'src={volume}></img>
-          <a href='speechTechniques'><img className='arrow2'src={arrow}/></a> 
-          </div>
-
-          <div className='quickPractice'>
-          <div className='rectangle2'></div>
-          <h6 className='activitytext'>Quick Practice</h6>
-          <img className='icon'src={mic}></img>
-          <a href='quickPractice'><img className='arrow2'src={arrow}/></a>
-          <h6 className='progress'>View Progress</h6>
-          </div>
+      <div>
+        <div className="dailyTasks">
+          <div className="rectangle"></div>
+          <h6 className="activitytext">Daily Activities</h6>
+          <img className="icon" src={fire}></img>
+          <a href="dailyActivities">
+            <img className="arrow2" src={arrow} />
+          </a>
         </div>
 
+        <div className="speechTechniques">
+          <div className="rectangle2"></div>
+          <h6 className="activitytext">Speech Techniques</h6>
+          <img className="icon" src={volume}></img>
+          <a href="speechTechniques">
+            <img className="arrow2" src={arrow} />
+          </a>
+        </div>
+
+        <div className="quickPractice">
+          <div className="rectangle2"></div>
+          <h6 className="activitytext">Quick Practice</h6>
+          <img className="icon" src={mic}></img>
+          <a href="quickPractice">
+            <img className="arrow2" src={arrow} />
+          </a>
+          <h6 className="progress">View Progress</h6>
+        </div>
+      </div>
     </div>
   );
 }
